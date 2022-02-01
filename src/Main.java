5303e89577f7a9255f8957f4f918f41d87640458
ek223gi <ek223gi@student.lnu.se>
2022-02-01 16:26:40 +0300

    Connection and statement created

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
 
 public class Main{
+    static final int portAddress = 3307;
+    static Connection connection = createConnection();
+    static Statement stmnt = createStatement();
+
     public static void main(String[] args) throws IOException {
 
         File jsonFile = new File("RC_2007-10");
@@ -23,6 +28,54 @@ public class Main{
             line = br.readLine();
             printValues(line, jsonKeys);
         }
+    }
+
+    public static Connection createConnection(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:" + portAddress + "/car_rental";
+            con = DriverManager.getConnection(url, "root", "root");
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static Statement createStatement(){
+        try{
+            stmnt = connection.createStatement();
+        } catch(Exception ex) {
+            System.out.println("Exception");
+        }
+        return stmnt;
+    }
+
+    static void insert() throws SQLException {
+
+            String id = "";
+            String parent_id = "";
+            String link_id = "";
+            String name = "";
+            String author = "";
+            String body = "";
+            String subreddit_id = "";
+            String subreddit = "";
+            int created_utc = 0;
+            int score = 0;
+
+//            String query = "INSERT INTO client (personal_number, firstname, lastname, address) VALUES (?, ?, ?, ?)";
+//
+//            PreparedStatement preparedStatement = connection.prepareStatement(query);
+//            preparedStatement.setInt(1, personalNumber);
+//            preparedStatement.setString(2, firstName);
+//            preparedStatement.setString(3, lastName);
+//            preparedStatement.setString(4, address);
+//
+//            preparedStatement.execute();
+
+
     }
 
     static void printValues(String line, String[] jsonKeys){
