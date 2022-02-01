e11349c00489816ec92c7f017b7be6bef0714f56
ma226fq <ma226fq@student.lnu.se>
2022-02-01 19:36:01 +0300

    Latest commit

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,21 @@ public class Main{
     static final int portAddress = 3307;
     static Connection connection = createConnection();
     static Statement stmnt = createStatement();
+    static final String[] jsonKeys = {
+            "id", "parent_id", "link_id", "name",
+            "author", "body", "subreddit_id",
+            "subreddit", "created_utc", "score"
+    };
 
     public static void main(String[] args) throws IOException {
 
         File jsonFile = new File("RC_2007-10");
         FileReader fr = new FileReader(jsonFile);
         BufferedReader br = new BufferedReader(fr);
-
-        String[] jsonKeys = {
-                "id", "parent_id", "link_id", "name",
-                "author", "body", "subreddit_id",
-                "subreddit", "created_utc",
-        };
-
         String line;
-        for (int i = 0; i < 20; i++){
-            line = br.readLine();
-            printValues(line, jsonKeys);
+
+        while ((line = br.readLine()) != null){
+            printValues(line);
         }
     }
 
@@ -34,7 +32,7 @@ public class Main{
         Connection con = null;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:" + portAddress + "/car_rental";
+            String url = "jdbc:mysql://localhost:" + portAddress + "/reddit";
             con = DriverManager.getConnection(url, "root", "root");
 
         } catch(Exception e) {
@@ -78,17 +76,19 @@ public class Main{
 
     }
 
-    static void printValues(String line, String[] jsonKeys){
+    static void printValues(String line){
 
         JSONObject obj = new JSONObject(line);
 
-        for (String key : jsonKeys){
-            System.out.println(key + ": " + obj.getString(key));
+        for (int i = 0; i < 9; i++){
+            System.out.println(jsonKeys[i] + ": " + obj.get(jsonKeys[i]));
         }
-        System.out.println("score: " + obj.getBigInteger( "score"));
+        System.out.println(jsonKeys[9] + ": " + obj.get(jsonKeys[9]));
+
 
         printDashes(100);
         System.out.println();
+
     }
 
     static void printDashes(int n){
