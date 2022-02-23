69260caae0ea50c801c71ea52c36bb078f37d1d0
emre-ast <53765541+emre-ast@users.noreply.github.com>
2022-02-23 16:37:20 +0300

    Add files via upload

--- reddit.sql
+++ reddit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 18, 2022 at 02:08 PM
+-- Generation Time: Feb 23, 2022 at 01:36 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.16
 
@@ -33,9 +33,9 @@ CREATE TABLE `comment` (
   `link_id` varchar(8) NOT NULL,
   `name` varchar(10) NOT NULL,
   `author` varchar(20) NOT NULL,
-  `body` blob NOT NULL,
+  `body` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `score` int(6) NOT NULL,
-  `created_utc` bigint(10) NOT NULL
+  `created_utc` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -57,7 +57,7 @@ CREATE TABLE `post` (
 
 CREATE TABLE `subreddit` (
   `id` varchar(10) NOT NULL,
-  `name` varchar(30) NOT NULL
+  `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -69,20 +69,23 @@ CREATE TABLE `subreddit` (
 --
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `link_id` (`link_id`);
+  ADD KEY `score_index` (`score`),
+  ADD KEY `author` (`author`,`link_id`) USING BTREE,
+  ADD KEY `link_id` (`link_id`,`created_utc`) USING BTREE;
 
 --
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `subreddit_id` (`subreddit_id`);
+  ADD KEY `subreddit_id` (`subreddit_id`) USING BTREE;
 
 --
 -- Indexes for table `subreddit`
 --
 ALTER TABLE `subreddit`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Constraints for dumped tables
